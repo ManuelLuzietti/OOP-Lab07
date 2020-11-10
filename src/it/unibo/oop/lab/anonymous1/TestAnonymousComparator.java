f6af80c3b5ec03bb97df98665db9d44e4ee60d63
Manuel <holamigos2015@gmail.com>
2020-11-10 16:16:49 +0100

    finito es senza documentazione

--- src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
+++ src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
@@ -1,6 +1,8 @@
 package it.unibo.oop.lab.anonymous1;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import it.unibo.oop.lab.socialnetwork.SocialNetworkUser;
@@ -75,6 +77,12 @@ public final class TestAnonymousComparator {
          * REFER TO LESSON 13-Advanced-Mechanisms.pdf, slide 41
          */
         // TODO
+        Collections.sort(denzelUsers,new  Comparator<User>(){
+			public int compare(User arg0, User arg1) {
+				return arg0.getAge() - arg1.getAge() ;
+			}
+			
+        });
         /*
          * expected Result
          */
@@ -106,6 +114,14 @@ public final class TestAnonymousComparator {
          * class Collections
          */
         // TODO
+        
+        Collections.sort(rossiUsers,  new Comparator<User>() {
+        	
+			public int compare(User arg0, User arg1) {
+				return arg1.getAge() - arg0.getAge();
+			}
+        	
+        });
         /*
          * expected Result
          */
